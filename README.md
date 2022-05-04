5ea008a9fd3235e6a4b670f9ac57c089e6ba5daa
Liza <liza140620002@gmail.com>
2022-05-04 06:49:09 +0300

    Add starter pack

---
+++ README.md
@@ -0,0 +1,11 @@
+## Задача
+
+#### Додати лоадер під час завантаження данних та додати можливість обирати улюбленних персонажів
+
+Імплементувати логіку для функції `changeFavourites` та додати лоадер під час завантаження данних
+
+## Task
+
+#### Add loader while data is been fetching and add ability to choose favourite characters
+
+Implement logic for the function `changeFavourites` and add loader data is been fetching
---
+++ app.js
@@ -0,0 +1,86 @@
+const findBtn = document.querySelector('.btn-find-сharacters');
+const сharacterContainer = document.querySelector('.сharacter-container');
+
+const changeFavourites = (e, characterId) => {
+  //todo: implement changeFavourite function
+};
+
+const createCharacterNode = ({
+  id,
+  fullName,
+  title,
+  imageUrl,
+  isFavourite = false,
+}) => {
+  const container = document.createElement('div');
+  container.classList.add('сharacter-card');
+
+  const avatar = document.createElement('img');
+  avatar.classList.add('сharacter-avatar');
+  avatar.setAttribute('src', imageUrl);
+
+  const bodyContainer = document.createElement('div');
+  bodyContainer.classList.add('card-body');
+
+  const fullNameItem = document.createElement('h4');
+  fullNameItem.classList.add('сharacter-fullname');
+  fullNameItem.innerText = `${fullName},`;
+
+  const titleItem = document.createElement('h5');
+  titleItem.classList.add('сharacter-title');
+  titleItem.innerText = title;
+
+  const reactionContainer = document.createElement('div');
+  reactionContainer.classList.add('reaction-container');
+
+  const reactionImage = document.createElement('img');
+  reactionImage.classList.add('reaction-img');
+  if (isFavourite) {
+    reactionImage.classList.add('favourite');
+  }
+  reactionImage.setAttribute(
+    'src',
+    `assets/images/${isFavourite ? 'fulled-heart.webp' : 'empty-heart.png'}`
+  );
+
+  reactionImage.addEventListener('click', (e) => changeFavourites(e, id));
+
+  bodyContainer.append(fullNameItem);
+  bodyContainer.append(titleItem);
+  reactionContainer.append(reactionImage);
+  container.append(avatar, reactionContainer, bodyContainer);
+  return container;
+};
+
+const forwardRenderCharactersByFilter = async () => {
+  //todo: add loader while fetching data
+  const localData = localStorage.getItem('favourites');
+  const favourites = localData ? JSON.parse(localData) : [];
+
+  сharacterContainer.replaceChildren();
+  const сharacterNodes = [];
+  const сharacterNameInput = document.querySelector('.сharacter-input');
+  const сharacters = await getCharacters(сharacterNameInput.value);
+  for (const { id, fullName, imageUrl, title } of сharacters) {
+    сharacterNodes.push(
+      createCharacterNode({
+        id,
+        fullName,
+        imageUrl,
+        title,
+        isFavourite: favourites.includes(id),
+      })
+    );
+  }
+  сharacterContainer.append(...сharacterNodes);
+};
+
+findBtn.addEventListener('click', forwardRenderCharactersByFilter);
+
+const getCharacters = async (сharacterNameInput = '') => {
+  const response = await fetch(
+    `https://my-got-api.herokuapp.com/${сharacterNameInput}`
+  );
+  const characters = await response.json();
+  return characters;
+};
